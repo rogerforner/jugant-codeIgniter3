1bdbae5b2b587a0f7baacb03eb67cc83863be155
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-03-04 12:40:13 +0100

    helpers

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,7 +8,7 @@
     <hr>
     <pre><?php print_r($names); ?></pre>
 	</div>
-
+  <?php echo base_url(); ?>
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
